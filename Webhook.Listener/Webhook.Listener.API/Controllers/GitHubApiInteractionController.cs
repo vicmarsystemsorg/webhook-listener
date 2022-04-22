25299abadf9bea4c8d018887b58a6c277445a491
Vicluar Merchan <vicmarsystems@gmail.com>
2022-04-23 01:59:40 +0200

    Testing the interaction with the Github API. Using Octokit library

---
+++ Webhook.Listener/Webhook.Listener.API/Controllers/GitHubApiInteractionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Octokit;
+
+namespace Webhook.Listener.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GitHubApiInteractionController : ControllerBase
+    {
+        [HttpGet("CreateWebHookCreateRepoEvent")]
+        public IActionResult CreateWebHookCreateRepoEvent()
+        {
+            
+
+            return new OkResult();
+        }
+
+        [HttpGet("TestingAPIInteraction")]
+        public async Task<IActionResult> TestingAPIInteraction()
+        {
+            var tokenAuth = new Credentials("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");
+
+            var client = new GitHubClient(new ProductHeaderValue("vicmarsystemsorg"));
+            client.Credentials = tokenAuth;
+
+            var data = await client.Repository.GetAllForCurrent();
+
+            return new OkObjectResult(data);
+        }
+    }
+}
