de0fda0dc91461a3a20c04f8e47cd5941e472af1
Vicluar Merchan <vicmarsystems@gmail.com>
2022-04-23 01:03:51 +0200

    Adding basic controller to receive the payload from Github webhook

---
+++ Webhook.Listener/Webhook.Listener.API/Controllers/PayloadController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Webhook.Listener.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class PayloadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Payload([FromBody] JsonElement payload)
+        {
+            return new OkResult();
+        }
+    }
+}
