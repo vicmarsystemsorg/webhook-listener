001102f646de8507247ee816fef37975b73998b7
Vicluar Merchan <vicmarsystems@gmail.com>
2022-04-23 03:29:49 +0200

    Changing the code to return a connection to make api requests.

--- Webhook.Listener/Webhook.Listener.API/Controllers/GitHubApiInteractionController.cs
+++ Webhook.Listener/Webhook.Listener.API/Controllers/GitHubApiInteractionController.cs
@@ -19,14 +19,22 @@ namespace Webhook.Listener.API.Controllers
         [HttpGet("TestingAPIInteraction")]
         public async Task<IActionResult> TestingAPIInteraction()
         {
-            var tokenAuth = new Credentials("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");
+            var connection = GetConnection();
+            var data = await connection.Get<HttpResponse>(new Uri("https://api.github.com/orgs/vicmarsystemsorg/hooks"), TimeSpan.FromSeconds(20));
+
+            return new OkObjectResult(data);
+        }
+
+        private IConnection GetConnection()
+        {
+            var tokenAuth = new Credentials("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");
 
             var client = new GitHubClient(new ProductHeaderValue("vicmarsystemsorg"));
             client.Credentials = tokenAuth;
 
-            var data = await client.Repository.GetAllForCurrent();
+            //var data = await client.Repository.GetAllForCurrent();
 
-            return new OkObjectResult(data);
+            return client.Connection;
         }
     }
 }
